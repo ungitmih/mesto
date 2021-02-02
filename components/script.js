046a72e399dc1a496e46886ba1fb3edfc7400ab1
MikhailB <unmihbob@yandex.ru>
2021-02-02 23:17:50 +0100

    mesto-final4

--- components/script.js
+++ components/script.js
@@ -2,15 +2,17 @@ let popup = document.querySelector(".popup");
 let buttonEdit = document.querySelector(".profile__edit-button");
 let buttonClose = popup.querySelector(".popup__close");
 let name = document.querySelector(".profile__name").textContent;
+document.querySelector('#profile-name').value = name;
 let job = document.querySelector(".profile__description").textContent;
+document.querySelector('#profile-description').value = job;
 let nameDiv = document.querySelector(".profile__name");
 let jobDiv = document.querySelector(".profile__description");
+let nameInput = document.querySelector('#profile-name');
+let jobInput = document.querySelector('#profile-description');
+let formElement = document.querySelector(".form");
 
 //открытие попапа
 function open() {
-document.querySelector('#profile-name').value = name;
-document.querySelector('#profile-description').value = job;
-
 popup.classList.add("popup_opened");
 }
 //закрытие попапа
@@ -18,9 +20,6 @@ function close() {
 popup.classList.remove("popup_opened");
 }
 
-let formElement = document.querySelector(".form");
-let nameInput = document.querySelector('#profile-name');
-let jobInput = document.querySelector('#profile-description');
 //отправка формы
 function formSubmitHandler(evt) {
 evt.preventDefault();
@@ -31,7 +30,7 @@ let jobInputValue = jobInput.value;
 nameDiv.textContent = nameInputValue;
 jobDiv.textContent = jobInputValue;
 
-return close();
+close();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
