a980a28d91899d3729cc7553e39ae53bd260ad4b
MikhailB <unmihbob@yandex.ru>
2021-03-14 23:28:48 +0100

    mesto-validation-thisstart

--- scripts/index.js
+++ scripts/index.js
@@ -24,6 +24,159 @@ const formElementPhoto = popupAddPhoto.querySelector(".form");
 const elementsItems = document.querySelector('.elements__items');
 const templateEl = document.querySelector('.elements-template');
 
+class Card {
+  constructor(cardSelector) {
+    this._cardSelector = cardSelector;
+  }
+
+_getTemplate() {
+  const cardElement = document
+    .querySelector(this._cardSelector)
+    .content
+    .querySelector('.element')
+    .cloneNode(true);
+
+  return cardElement;
+}
+
+_setEventListeners() {
+  this._element.addEventListener('click', () => {
+    this._handleOpenPopup();
+  });
+}
+}
+
+class DefaultCard extends Card {
+  constructor(data, cardSelector) {
+    super(cardSelector);
+    this._title = data.title;
+    this._description = data.description;
+    this._image = data.image;
+  }
+
+  generateCard() {
+    this._element = super._getTemplate();
+    super._setEventListeners();
+
+    this._element.querySelector('.element__picture').src = this._image;
+    this._element.querySelector('.element__title').alt = this._title;
+
+    return this._element;
+  }
+}
+
+const renderElements = () => {
+  elementsItems.innerHTML = '';
+  initialCards.forEach((item) => {
+    const card = new DefaultCard(item, '.elements-template');
+    const cardElement = card.generateCard();
+    elementsItems.append(cardElement);
+  });
+};
+
+renderElements(true);
+
+/*
+class Card {
+  constructor(data, templateSelector, settings, ownerId, { handleCardClick, handleDeleteCardClick, setLike, deleteLike }) {
+    this._data = data;
+    this._templateSelector = templateSelector;
+    this._settings = settings;
+    this._ownerId = ownerId
+    this._handleCardClick = handleCardClick;
+    this._handleDeleteCardClick = handleDeleteCardClick;
+    this._setLike = setLike;
+    this._deleteLike = deleteLike;
+  }
+
+  _getTemplateElement() {
+    const cardElement = document
+      .querySelector(this._templateSelector)
+      .content
+      .firstElementChild
+      .cloneNode(true);
+    return cardElement;
+  }
+
+  deleteCard() {
+    this._deleteElem(this._element);
+  }
+
+  _deleteElem(elem) {
+    elem.remove();
+    elem = null;
+  }
+
+  _dislike(data) {
+    this._removeLikedClass();
+    this._deleteLike(data);
+  }
+
+  _like(data) {
+    this._addLikedClass();
+    this._setLike(data);
+  }
+
+  _removeLikedClass() {
+    this._likeButton.classList.remove(this._settings.photoLikedButtonClass);
+  }
+
+  _addLikedClass() {
+    this._likeButton.classList.add(this._settings.photoLikedButtonClass);
+  }
+
+  setLikeCount(data) {
+    this._photoLikeCount.textContent = String(data.likes.length);
+  }
+
+  _checkIsOwnCard() {
+    if (this._data.owner._id !== this._ownerId) {
+      this._deleteElem(this._deleteButton);
+    }
+  }
+
+  _checkLikedState() {
+    this._data.likes.forEach((likeOwner) => {
+      if (likeOwner._id === this._ownerId) {
+        this._addLikedClass();
+      }
+    })
+  }
+
+  _setEventListeners() {
+    this._photoImage.addEventListener('click', () => {
+      this._handleCardClick(this._data);
+    })
+    this._likeButton.addEventListener('click', () => {
+      if (this._likeButton.classList.contains(this._settings.photoLikedButtonClass)) {
+        this._dislike(this._data);
+      } else {
+        this._like(this._data);
+      }
+    })
+    this._deleteButton.addEventListener('click', this._handleDeleteCardClick);
+  }
+
+  generateCard() {
+    this._element = this._getTemplateElement();
+    this._photoImage = this._element.querySelector(this._settings.photoImageSelector);
+    this._photoFigcaption = this._element.querySelector(this._settings.photoFigcaptionSelector);
+    this._likeButton = this._element.querySelector(this._settings.photoLikeButtonSelector);
+    this._photoLikeCount = this._element.querySelector(this._settings.photoLikeCountSelector);
+    this._deleteButton = this._element.querySelector(this._settings.photoDeleteButtonSelector);
+    this._element.setAttribute('id', `a${this._data._id}`);
+    this._photoImage.src = this._data.link;
+    this._photoImage.alt = `Фотография ${this._data.name}`;
+    this._photoFigcaption.textContent = this._data.name;
+    this.setLikeCount(this._data)
+    this._setEventListeners();
+    this._checkIsOwnCard();
+    this._checkLikedState();
+    return this._element;
+  }
+}
+
+
 //вывод карточек
 function renderCard() {
   const html = initialCards
@@ -63,6 +216,7 @@ function createCard(item) {
 }
 
 renderCard();
+*/
 
 //открытие попапов
 const openPopup = (popup) => {
--- scripts/validate.js
+++ scripts/validate.js
@@ -7,6 +7,90 @@ const formValidation = {
   errorClass: 'form__error'
 };
 
+class FormValidator {
+  constructor(formElement, openFormButtons, settings) {
+    this._formElement = formElement;
+    this._settings = settings;
+    this._openFormButtons = openFormButtons;
+  }
+  _getErrorElement(inputElement) {
+    return this._formElement.querySelector(`#${inputElement.id}-error`);
+  }
+  _showInputError(inputElement, validationMessage, errorElement) {
+    inputElement.classList.add(this._settings.inputErrorClass);
+    errorElement.textContent = validationMessage;
+    errorElement.classList.add(this._settings.errorClass);
+  }
+  _hideInputError(inputElement, errorElement) {
+    inputElement.classList.remove(this._settings.inputErrorClass);
+    errorElement.textContent = '';
+    errorElement.classList.remove(this._settings.errorClass);
+  }
+  _hasInvalidInput() {
+    return this._formInputs.some(inputElement => {
+      return !inputElement.validity.valid;
+    })
+  }
+  _checkInputValidity(inputElement) {
+    const errorElement = this._getErrorElement(inputElement);
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, inputElement.validationMessage, errorElement);
+    } else {
+      this._hideInputError(inputElement, errorElement);
+    }
+  }
+  _toggleSubmitButtonState() {
+    if (this._hasInvalidInput(this._formInputs)) {
+      this._formSubmitButton.disabled = true;
+      this._formSubmitButton.classList.add(this._settings.inactiveButtonClass);
+    } else {
+      this._formSubmitButton.disabled = false;
+      this._formSubmitButton.classList.remove(this._settings.inactiveButtonClass);
+    }
+  }
+  _inputEventListener(evt) {
+    const inputElement = evt.target;
+    this._checkInputValidity(inputElement);
+    this._toggleSubmitButtonState();
+  }
+
+  _hideErrorMessages() {
+    this._inputsList = Array.from(this._formElement.querySelectorAll(this._settings.inputSelector));
+    this._inputsList.forEach(element => {
+      element.classList.remove(this._settings.inputErrorClass);
+    })
+    this._errorsList = Array.from(this._formElement.querySelectorAll(`${this._settings.inputSelector}-error`));
+    this._errorsList.forEach(element => {
+      element.textContent = '';
+      element.classList.remove(this._settings.errorClass);
+    })
+  }
+
+  _setEventListeners() {
+    this._formElement.addEventListener('reset', () => {
+      this._hideErrorMessages();
+    })
+    this._formInputs = Array.from(this._formElement.querySelectorAll(this._settings.inputSelector));
+    this._formSubmitButton = this._formElement.querySelector(this._settings.submitButtonSelector);
+    this._openFormButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        this._toggleSubmitButtonState();
+      })
+    })
+    this._toggleSubmitButtonState();
+    this._formInputs.forEach(inputElement => {
+      inputElement.addEventListener('input', (evt) => {
+        this._inputEventListener(evt);
+      });
+    })
+  }
+
+  enableValidation() {
+    this._setEventListeners();
+  }
+}
+
+/*
 //показать сообщение об ошибке
 const showError = (formElement, inputElement, errorMessage, inputErrorClass, errorClass) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
@@ -80,3 +164,4 @@ const enableValidation = ({
 }
 
 enableValidation(formValidation);
+*/
